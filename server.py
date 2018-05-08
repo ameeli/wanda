a4cdd91d63ed62103ebadb4de0cacd20a87fccf8
vagrant <vagrant@vagrant.vm>
2018-05-08 22:52:01 +0000

    Modified register route to text user once successfully registered.

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
-from flask import Flask, request, session, render_template, redirect, session, flash
-from twilio.twiml.messaging_response import MessagingResponse
-
+# import flask libraries
+from flask import Flask, flash, session
+from flask import request, render_template, redirect
 # from flask_debugtoolbar import DebugToolbarExtension
 
+# download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+
+# import objects and classes from model.py
 from model import connect_to_db, db, User
+
+import os
+# my Account Sid and Auth Token from twilio.com/console
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
 
 app = Flask(__name__)
 app.secret_key = 'KEY'
@@ -14,7 +25,6 @@ def display_homepage():
     """Display homepage."""
 
     return render_template("homepage.html")
-        
 
 
 @app.route('/register', methods=['GET'])
@@ -28,23 +38,30 @@ def show_signup_form():
 def add_user():
     """Adds new user to database."""
 
-    # get form variables
+    # get form variables from register_form.html
     fname = request.form['fname']
     lname = request.form['lname']
     email = request.form['email']
     password = request.form['password']
     mobile = request.form['mobile']
 
+    # instantiate an instance of User
     user = User(fname=fname, 
                 lname=lname, 
                 email=email, 
                 password=password, 
                 mobile=mobile)
 
+    # add user to db
     db.session.add(user)
     db.session.commit()
 
     flash("Wanda welcomes you!")
+
+    # send confirmation text to user's mobile using Twilio
+    client.messages.create(body="Wanda warmly welcomes you!",
+                           from_="+14159156178",
+                           to=mobile)
 
     return redirect('/home') # edit redirect to profile page
 
@@ -79,6 +96,7 @@ def hello():
     return "Hello world!"
 
 
+# this route is an example from Twilio of how to respond to a user text
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
