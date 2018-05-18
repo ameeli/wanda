1423cf529777be8dbc03580390d3ced48ec5c7ff
vagrant <vagrant@vagrant.vm>
2018-05-18 20:00:11 +0000

    Created to feed server routes json data.

---
+++ src/chart_data.py
@@ -0,0 +1,109 @@
+pie_data = [{
+             'mw': 'mindwandering',
+             'occurrence': 50
+            },
+
+            {
+             'mw': 'not wandering',
+             'occurrence': 62
+            }]
+
+mw_graph_data = [{
+                  'letter': '1',
+                  'frequency': 0.0005
+                 },
+
+                 {
+                  'letter': '2',
+                  'frequency': 0.0005
+                 },
+
+                 {
+                  'letter': '3',
+                  'frequency': 0.04
+                 },
+
+                 {
+                  'letter': '4',
+                  'frequency': 0.04
+                 },
+
+                 {
+                  'letter': '5',
+                  'frequency': 0.16
+                 },
+                
+                 {
+                  'letter': '6',
+                  'frequency': 0.42
+                 },
+
+                 {
+                  'letter': '7',
+                  'frequency': 0.26
+                 },
+
+                 {
+                  'letter': '8',
+                  'frequency': 0.06
+                 },
+
+                 {
+                  'letter': '9',
+                  'frequency': 0.02
+                 },
+
+                 {
+                  'letter': '10',
+                  'frequency': 0.0005
+                 }]
+
+not_mw_graph_data = [{
+                  'letter': '1',
+                  'frequency': 0.0005
+                 },
+
+                 {
+                  'letter': '2',
+                  'frequency': 0.016
+                 },
+
+                 {
+                  'letter': '3',
+                  'frequency': 0.0005
+                 },
+
+                 {
+                  'letter': '4',
+                  'frequency': 0.016
+                 },
+
+                 {
+                  'letter': '5',
+                  'frequency': 0.096
+                 },
+                
+                 {
+                  'letter': '6',
+                  'frequency': 0.258
+                 },
+
+                 {
+                  'letter': '7',
+                  'frequency': 0.290
+                 },
+
+                 {
+                  'letter': '8',
+                  'frequency': 0.226
+                 },
+
+                 {
+                  'letter': '9',
+                  'frequency': 0.080
+                 },
+
+                 {
+                  'letter': '10',
+                  'frequency': 0.016
+                 }]
